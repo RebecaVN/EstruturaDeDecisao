6927a41f6c5233873b246f575a99dc8a4c0c535c
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 09:45:11 -0300

    logica.java

---
+++ logica.java
@@ -0,0 +1,22 @@
+package EstruturaDecisaoLogica;
+
+public class Logica {
+	
+	
+	public static void main(String[]args) {
+		
+		int numero1 = -6;
+		int numero2 = -5;
+		int numero3 =  8;
+		int numero4 =  9;
+		
+		int numeroTeste = numero4;
+		
+		if(numeroTeste > 0 & numeroTeste%2 == 0) {
+			System.out.println("O número é positivo e divisível por 2");
+		}else {
+			System.out.println("Não é positivo ou não é par");
+		}
+				
+	}
+}
